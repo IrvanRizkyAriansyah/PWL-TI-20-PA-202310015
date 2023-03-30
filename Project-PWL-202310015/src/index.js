7f2dd2cd23dfd8614fe3532f100fc0e095a3e651
IrvanRizkyAriansyah <irvanrizky58@gmail.com>
2023-03-30 22:09:54 +0700

    tugas 3

--- Project-PWL-202310015/src/index.js
+++ Project-PWL-202310015/src/index.js
@@ -3,18 +3,30 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import FormPersonalData from "./components/tugas1/FormPersonalData";
 import Widget1 from "./components/tugas2/Widget1";
 import FormPersonalDataMultiple from "./components/tugas2/FormPersonalDataMultiple";
+import Layout from "./components/pertemuan3/layouts/Layout";
+import Homes from "./components/pertemuan3/modules/Homes/Homes";
+import AppRoute from "./components/pertemuan3/apps/AppRoute";
+import { BrowserRouter } from "react-router-dom";
+
+const { PUBLIC_URL } = process.env;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <FormPersonalData />
+    {/* <FormPersonalData />
     <Widget1 />
-    <FormPersonalDataMultiple />
+    <FormPersonalDataMultiple /> */}
+    {/* <Layout>
+      <Homes />
+    </Layout> */}
+
+    <BrowserRouter basename={PUBLIC_URL}>
+      <AppRoute />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
